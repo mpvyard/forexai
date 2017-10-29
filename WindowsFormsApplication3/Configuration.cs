436b0fb2b0383052ddf9a9e5aa223a8ad573e09d
deconf <deconf@ya.ru>
2017-10-29 22:24:22 +0300

    fix sleep bigger

--- WindowsFormsApplication3/Configuration.cs
+++ WindowsFormsApplication3/Configuration.cs
@@ -19,7 +19,7 @@ namespace FinancePermutator
 		public const int TestDataAmountPerc = 6;
 		public const int TrainLimitEpochs = 200;
 		public static uint DefaultHiddenNeurons = 0;
-		public const int SleepTime = 120;
+		public const int SleepTime = 200;
 		public const int SleepCheckTime = 10000;
 
 		public const TrainingAlgorithm TrainAlgo = TrainingAlgorithm.TRAIN_RPROP; //TRAIN_QUICKPROP;//TRAIN_RPROP;

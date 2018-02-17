20a85466b7f8d9e2dea1757fd3901143c13a737a
deconf <deconf@ya.ru>
2018-02-17 09:28:15 +0300

    dev

--- WindowsFormsApplication3/Configuration.cs
+++ WindowsFormsApplication3/Configuration.cs
@@ -1,28 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using FANNCSharp;
-using FANNCSharp.Double;
+﻿using FANNCSharp;
 
 namespace FinancePermutator
 {
-	public static class Configuration
-	{
-		public const string PriceFileName = @"d:\temp\forexAI\GBPUSD.csv";
-		public const string LogFileName = @"d:\temp\forexAI\forexAI.log";
-		public const int InputDimension = 64;
-		public const double MinSaveTestMSE = 0.02;
-		public const double MinSaveHit = 90;
-		public const int MinSaveEpoch = 20;
-		public const int MinTaFunctionsCount = 3;
-		public const int TestDataAmountPerc = 6;
-		public const int TrainLimitEpochs = 650;
-		public static uint DefaultHiddenNeurons = 0;
-		public const int SleepTime = 300;
-		public const int SleepCheckTime = 10000;
-		public const int OutputIndex = 5;
-		public const TrainingAlgorithm TrainAlgo = TrainingAlgorithm.TRAIN_RPROP;
-	}
+    public static class Configuration
+    {
+        public const string PriceFileName = @"d:\temp\forexAI\GBPUSD15.csv";
+        public const string LogFileName = @"d:\temp\forexAI\forexAI.log";
+        public const int InputDimension = 64;
+        public const double MinSaveTestMSE = 0.02;
+        public const double MinSaveHit = 89;
+        public const int MinSaveEpoch = 20;
+        public const int MinTaFunctionsCount = 3;
+        public const int TestDataAmountPerc = 6;
+        public const int TrainLimitEpochs = 650;
+        public static uint DefaultHiddenNeurons = 0;
+        public const int SleepTime = 300;
+        public const int SleepCheckTime = 10000;
+        public const int OutputIndex = 5;
+        public const TrainingAlgorithm TrainAlgo = TrainingAlgorithm.TRAIN_RPROP;
+    }
 }
